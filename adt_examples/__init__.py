4094fcdef907ff2f821946fecbd6e7de40d14059
gavinconran <gtconran@gmail.com>
2024-12-09 19:51:20 +0000

    package adt_examples

---
+++ adt_examples/__init__.py
@@ -0,0 +1,2 @@
+"""Contains the adt-examples __init__ file Ex 5 of OOP4Maths."""
+from adt_examples.fibonacci import Fib # noqa F401
---
+++ adt_examples/fibonacci.py
@@ -0,0 +1,31 @@
+"""Contains the class Fib for Ex 5 of OOP4Maths."""
+
+
+class Fib:
+    """The Fib class represents a fibonaci number."""
+
+    def __init__(self, fib_o=0, fib_1=1, next=None):
+        """Fib class constructor method."""
+        self.value = value
+        self.next = next
+
+    def __iter__(self):
+        return FibIterator(self)
+
+
+ class FibIterator:
+        """Concrete implementation of the iterator protocol"""
+    def __iter__(self, link):
+        self.here = link
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        next = self.here
+        f_1 = next.fib_0
+        f_2 = next.fib_1
+        next.fib_0 = f_1
+        next.fib_1 = f_1 + f_2
+        return next.fib_0 + next.fib_1
+            
