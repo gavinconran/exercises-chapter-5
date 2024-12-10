5882c41dcbf570a6effab0ddf7a0e055cb240135
gavinconran <gtconran@gmail.com>
2024-12-10 16:34:13 +0000

    iterator issues v3

--- adt_examples/deque.py
+++ adt_examples/deque.py
@@ -71,6 +71,6 @@ class DequeIterator:
         if self.here:
             next = self.here
             self.here = self.here.next
-            return next.list[:next.length][0]
+            return next.list[next.length]
         else:
             raise StopIteration
