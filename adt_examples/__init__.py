bf17b0281e41a2b7f2806503e0e24785168ad0d0
gavinconran <gtconran@gmail.com>
2024-12-14 17:38:37 +0000

    relative import in fibonacci, rplcalc and deque module __init__.py

--- adt_examples/__init__.py
+++ adt_examples/__init__.py
@@ -1,4 +1,4 @@
 """Contains the adt-examples __init__ file Ex 5 of OOP4Maths."""
-from adt_examples.fibonacci import Fib # noqa F401
-from adt_examples.rpcalc import RPCalc # noqa F401
-from adt_examples.deque import Deque # noqa F401
+from .fibonacci import Fib # noqa F401
+from .rpcalc import RPCalc # noqa F401
+from .deque import Deque # noqa F401
